7aac6413d7e72f51cdb5e884abc2bd630bcb675a
kreativerohail <rohail.codes@gmail.com>
2026-01-26 08:08:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -16,4 +16,15 @@ app.use(express.json());
 // Routes
 app.use("/api/contact", contactRoute);
 
+app.get("/api/testdb", async (req, res) => {
+  try {
+    await connectDB();
+    res.json({ success: true, message: "MongoDB connected!" });
+  } catch (err) {
+    console.error("MongoDB test error:", err);
+    res.status(500).json({ success: false, message: "DB connection failed" });
+  }
+});
+
+
 export default app;
