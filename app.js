2abfb4d7dcd27d67958372bba1d2707beab6d730
kreativerohail <rohail.codes@gmail.com>
2026-01-26 14:17:34 +0000

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./lib/db.js";
 import contactRoute from "./routes/contact.js";
 
 dotenv.config();
 
 const app = express();
 
-// Simple CORS: allow all origins
-app.use(cors());
+// 🔹 Connect MongoDB
+connectDB();
 
-// Body parser
+// Middleware
+app.use(cors());
 app.use(express.json());
 
 // Routes
 app.use("/api/contact", contactRoute);
-
 
 export default app;
